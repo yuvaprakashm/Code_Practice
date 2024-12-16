07fa18c37b4ff1bbacea88dd980d568ffa02595e
yuvaprakash <103112539+yuvaprakashm@users.noreply.github.com>
2024-12-16 10:57:24 +0530

    Update RemoveDuplicates.java

--- RemoveDuplicatesInArray/src/com/yuva/RemoveDuplicates.java
+++ RemoveDuplicatesInArray/src/com/yuva/RemoveDuplicates.java
@@ -1,18 +1,26 @@
 package com.yuva;
 
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedHashSet;
 
 public class RemoveDuplicates {
 	public static void main(String[] args) {
 		int[] array = { 1, 2, 3, 5, 2, 1, 3, 5 };
-		LinkedHashSet<Integer> set = new LinkedHashSet<>();
+//		LinkedHashSet<Integer> set = new LinkedHashSet<>();
+//		for (int num : array) {
+//			set.add(num);
+//		}
+//		Iterator<Integer> itr = set.iterator();
+//		while (itr.hasNext()) {
+//			System.out.print(itr.next());
+//		}
+		HashSet<Integer> seen = new HashSet<>();
+		System.out.println("Duplicate elements :");
 		for (int num : array) {
-			set.add(num);
-		}
-		Iterator<Integer> itr = set.iterator();
-		while (itr.hasNext()) {
-			System.out.print(itr.next());
+			if (!seen.add(num)) {
+				System.out.print(num);
+			}
 		}
 	}
 }
