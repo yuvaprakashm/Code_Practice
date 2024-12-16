651685a0dc4c42280e2384b5194e8a02bbc73b33
yuvaprakash <103112539+yuvaprakashm@users.noreply.github.com>
2024-12-16 10:55:25 +0530

    Update FibonacciSeries.java

--- FibinacciSeries/src/com/yuva/FibonacciSeries.java
+++ FibinacciSeries/src/com/yuva/FibonacciSeries.java
@@ -3,18 +3,18 @@ package com.yuva;
 public class FibonacciSeries {
 	public static void main(String[] args) {
 		int count = 10;
-		int num1 = 0;
-		int num2 = 1;
+		int a = 0;
+		int b = 1;
 
-		System.out.print(num1);
-		System.out.print(" " + num2);
+		System.out.print(a);
+		System.out.print(" " + b);
 
 		for (int i = 2; i < count; i++) {
 
-			int num3 = num1 + num2;
-			System.out.print(" " + num3);
-			num1 = num2;
-			num2 = num3;
+			int c = a + b;
+			System.out.print(" " + c);
+			a = b;
+			b = c;
 
 		}
 	}
